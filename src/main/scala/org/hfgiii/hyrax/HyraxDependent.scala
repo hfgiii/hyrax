0da03c474b156810076ac41aa95343bd836a6eb7
Hugh Gilmore <Hugh_Gilmore@s5a.com>
2013-07-13 07:33:11 -0400

    Just getting started

---
+++ src/main/scala/org/hfgiii/hyrax/HyraxDependent.scala
@@ -0,0 +1,21 @@
+package org.hfgiii.hyrax
+
+import akka.pattern.CircuitBreaker
+/*
+ * File: HyraxDependent.scala 
+ * Date: 7/8/13
+ * Time: 7:30 AM	
+ *
+ * Copyright (c) Saks Fifth Avenue.
+ * 12 East 49th Street, New York City, New York, 10017, U.S.A.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Saks
+ * Fifth Avenue. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Saks.
+ */
+class HyraxDependent {
+
+}
