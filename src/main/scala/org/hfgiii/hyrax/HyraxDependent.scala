4e68377ef6833cabd1e622ac0e44623ccf36649f
hgilmore <hgilmore@underarmour.com>
2014-11-25 16:16:55 -0500

    Removed non-public copyright from HyraxDependent.scala

--- src/main/scala/org/hfgiii/hyrax/HyraxDependent.scala
+++ src/main/scala/org/hfgiii/hyrax/HyraxDependent.scala
@@ -6,17 +6,6 @@ import akka.pattern.CircuitBreaker
  * Date: 7/8/13
  * Time: 7:30 AM	
  *
- * Copyright (c) Saks Fifth Avenue.
- * 12 East 49th Street, New York City, New York, 10017, U.S.A.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of Saks
- * Fifth Avenue. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Saks.
- *
- */
 trait HyraxDependent {
              /*
                  insure (10) of  {
